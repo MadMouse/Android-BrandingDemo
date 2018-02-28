f692263d26332abb00af9cc2afa2d97483a545d2
Leslie Drewery <pocketpicasa@googlemail.com>
2018-02-28 11:40:41 +0000

    White-label Example

---
+++ WhiteLabel/app/src/main/java/uk/co/madmouse/whitelabel/MainKotlin.kt
@@ -0,0 +1,6 @@
+package uk.co.madmouse.whitelabel
+
+/**
+ * Created by leslied on 27/02/2018.
+ */
+
---
+++ WhiteLabelKotlin/app/src/main/java/uk/co/madmouse/whitelabelkotlin/SvcTEst.kt
@@ -0,0 +1,18 @@
+package uk.co.madmouse.whitelabelkotlin
+
+import android.app.IntentService
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+ * Created by leslied on 27/02/2018.
+ */
+class SvcTEst : Service() {
+
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
